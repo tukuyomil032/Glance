f4e53b8892589824f78ba77244d54e2db8fc9b89
tukuyomil032 <yinkom0708@gmail.com>
2026-05-09 13:45:44 +0900

    Initial Commit

---
+++ glance/glanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  glanceApp.swift
+//  glance
+//
+//  Created by yomi on 2026/05/09.
+//
+
+import SwiftUI
+
+@main
+struct glanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ glanceTests/glanceTests.swift
@@ -0,0 +1,17 @@
+//
+//  glanceTests.swift
+//  glanceTests
+//
+//  Created by yomi on 2026/05/09.
+//
+
+import Testing
+@testable import glance
+
+struct glanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
